65fe615ecdca54e6d9e3362768049701561fca6d
molly12345molly <809612945@qq.com>
2020-05-22 23:04:35 +0800

    新增五行代码

--- src/Singleton/Hello.java
+++ src/Singleton/Hello.java
@@ -4,5 +4,11 @@ public class Hello {
     public static void main(String [] args){
         System.out.println("people");
         System.out.println("kill");
+        System.out.println("kill");
+        System.out.println("kill");
+        System.out.println("kill");
+        System.out.println("kill");
+        System.out.println("kill");
+        System.out.println("kill");
     }
 }
