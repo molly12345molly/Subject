7518c7eb0d33796e34894e09f9846312f36a05e4
molly12345molly <809612945@qq.com>
2020-05-22 23:49:38 +0800

    抽象工厂

---
+++ src/AbstractFactory/AbstractFactory.java
@@ -0,0 +1,7 @@
+package AbstractFactory;
+
+public abstract class AbstractFactory {
+
+    public abstract  Color getColor(String color);
+    public abstract  Shape getShape(String Shape);
+}
---
+++ src/AbstractFactory/Color.java
@@ -0,0 +1,6 @@
+package AbstractFactory;
+
+public interface Color {
+  void fill();
+
+}
