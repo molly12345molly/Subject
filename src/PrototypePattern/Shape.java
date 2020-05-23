21d9aa0e685f7cd2db49977470890dbf7f5db5c7
molly12345molly <809612945@qq.com>
2020-05-23 17:25:18 +0800

    原型模式更新意图

--- src/PrototypePattern/Shape.java
+++ src/PrototypePattern/Shape.java
@@ -1,6 +1,7 @@
 package PrototypePattern;
 //用原型实例指定创建对象的种类，并且通过拷贝这些原型创建新的对象。
 public abstract class Shape implements Cloneable {
+//    1.（抽象类或者接口）实现 java.lang.Cloneable 接口
     private String id;
     protected String type;
     abstract void draw();
@@ -20,6 +21,7 @@ public abstract class Shape implements Cloneable {
     public void setType(String type) {
         this.type = type;
     }
+    // 2.定义复制现有实例来生成新实例的方法
     public  Object clone(){
         Object clone=null;
         try {
